941bc61450a750df5cf76c43f2a0cdfc5e58e5a5
Josh Cooper <hi@joshc.uk>
2023-01-12 12:33:40 +0000

    Finished Onboarding

--- Assets/Scripts/OnboardingManager.cs
+++ Assets/Scripts/OnboardingManager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 using ARLogger;
+using TMPro;
 
 public class OnboardingManager : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public GameObject videoUiObject;
+    public TextMeshProUGUI uiHint;
 
     public VideoClip findAPlane;
     public VideoClip tapToPlace;
@@ -23,25 +25,34 @@ public class OnboardingManager : MonoBehaviour
 
     IEnumerator Onboarding()
     {
+        videoPlayer.Play();
+        videoPlayer.clip = findAPlane;
+
         // Start
         videoUiObject.SetActive(true);
+        uiHint.text = "Keep an eye on your surroundings!";
         LogManager.Instance.LogInfo("Starting Onboarding!");
-        videoPlayer.Play();
+        yield return new WaitForSeconds(3);
 
         // Find A Plane
-        videoPlayer.clip = findAPlane;
+        uiHint.text = "Find a Table...";
         LogManager.Instance.LogInfo("Playing 'Find a Plane'");
         yield return new WaitForSeconds(5);
 
         // Tap To Plane
         videoPlayer.clip = tapToPlace;
+        uiHint.text = "Tap on the Table to Place Your Dish!";
         LogManager.Instance.LogInfo("Playing 'Tap to Place'");
         yield return new WaitForSeconds(5);
 
         // End
         videoPlayer.clip = null;
+        uiHint.text = "Tap on the Dish to View More";
         LogManager.Instance.LogInfo("All Video's Played!");
         videoUiObject.SetActive(false);
+        yield return new WaitForSeconds(5);
+
+        uiHint.text = "";
     }
 
 }
